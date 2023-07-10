7e3257c36ccf59a5e00c6aa500da9941aad9c73d
Shubhashit <shubhashitjain@gmail.com>
2023-07-10 18:57:30 +0530

    cart is complete

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+    console.log('at least this is runneing')
     if (token == null) {
         return res.sendStatus(401);
     }
--- router/cart.js
+++ router/cart.js
@@ -12,42 +12,89 @@ router.post('/cart/add', authenticateToken, async (req, res) => {
         const userId = req.user.userId;
         const productId = req.body.productId;
         const quantity = req.body.quantity;
-        const product = await Product.findById(productId)
-        const findCart = await Cart.findOne({userId : userId})
+        const findCart = await Cart.findOne({ userId: userId })
         const currentDate = new Date();
-        
-        const num = 134123;
-        const insertProduct = {
-            productId,
-            quantity
-        }
-        console.log('check')
-        console.log(findCart)
-        if(findCart){
-            console.log('here int the ab9ove block')
-            findCart.f
-            await findCart.updateOne({
-                $push : {products : insertProduct}
-            })
+
+        if (findCart) {
+            const findCartwithProductId = await Cart.findOne({ userId: userId ,  'products.productId': productId });
+            if (findCartwithProductId) {
+                const index = findCartwithProductId.products.findIndex((product) => product.productId.toString() === productId);
+                console.log('find the prodcut');
+                console.log(findCartwithProductId)
+                await findCartwithProductId.updateOne({
+                    $set :{
+                        [`products.${index}.quantity`] : quantity 
+                    }
+                })
+                res.status(201).json({ message: 'product updated successfully' });
+            }
+            else {
+                await findCart.updateOne({
+                    $push: {
+                        products: {
+                            productId,
+                            quantity
+                        }
+                    }
+                })
+                res.status(201).json({ message: 'product saved successfully' });
+            }
             
         }
-        else{
+        else {
             console.log('here')
             const newcart = new Cart({
                 userId,
-                products : [{
+                products: [{
                     productId,
                     quantity
                 }],
-                createdAt : currentDate
+                createdAt: currentDate
             })
             await newcart.save();
+            res.status(201).json({ message: 'cart created successfully' });
         }
-        res.status(201).json({ message: 'cart saved successfully',  });
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({ error: err });
     }
 })
+
+router.get('/cart', authenticateToken , async (req, res) => {
+        try {
+            const  userId  = req.user.userId;
+            // Find the user's cart document
+            const cart = await Cart.findOne({ userId });
+
+            if (!cart) {
+                res.status(404).json({ error: 'Cart not found.' });
+            } else {
+                res.status(200).json({ cart });
+            }
+        } catch (err) {
+            console.error('Error retrieving cart:', err);
+            res.status(500).json({ error: 'An error occurred while retrieving the cart.' });
+        }
+})
+
+router.delete('/cart/delete/:productId' , authenticateToken , async(req, res)=> {
+    try {
+        console.log(req.user)
+        const userId = req.user.userId;
+        const productId = req.params.productId;
+        const findcart = await Cart.findOneAndUpdate({ userId : userId, 'products.productId': productId } , {
+            $pull : {
+                products : {productId}
+            }
+        } );
+
+
+        res.status(201).json({ message: 'product deleted successfully' });
+        
+    } catch (error) {
+        res.status(500).json({message : "error occurred" , error})
+    }
+})
+
 
 module.exports = router
